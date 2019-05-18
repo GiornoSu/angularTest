ff836a78d251f3d5fd25c052f8b3c1f102778cbb
Giorno Su <giorno_k1212@hotmail.com>
2019-05-18 10:59:18 +0800

    My to do list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { identifierModuleUrl } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'angular-test';
+  myList = [{ focus: false, title: "Hit the gym" }, { focus: true, title: "Pay bills" }, { focus: false, title: "Meet George" }, { focus: false, title: "Buy eggs" }, { focus: false, title: "Read a book" }, { focus: false, title: "Organize office" }];
+  clickItem = function (item : any) {
+    item.focus = !item.focus;
+  };
+  addItem = function (title : string) {
+    this.myList.push({ focus: false, title: title });
+  };
+  removeItem = function (item : any) {
+    this.myList.splice(this.myList.indexOf(item), 1);
+  };
 }
